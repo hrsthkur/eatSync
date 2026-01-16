e475399b49028e4a6ebbb64df9fccd1991f549a2
Harsh Singh <hrsthkur1@gmail.com>
2026-01-16 23:16:19 +0530

    db: add order model

---
+++ server/models/order.model.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose'
+
+
+const shopOrderItemSchema = new mongoose.Schema({
+    item: {
+         type:mongoose.Schema.Types.ObjectId,
+         ref:"Item"
+    },
+    price:Number,
+    quantity:Number
+},{timestamps: true})
+const shopOrderSchema = new mongoose.Schema({
+    shop:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Shop"
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    subtotal:Number,
+    shopOrderItem:[shopOrderItemSchema]
+
+},{timestamps:true})
+
+const orderSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    paymentMethod:{
+        type:String,
+        enum:['cod','online'],
+        required:true
+    },
+    deliveryAddress:{
+        text:String,
+        latitude:Number,
+        longitude:Number
+
+    },
+    totalAmount:{
+        type:Number
+    },
+    shopOrder:[shopOrderSchema]
+},{timestamps:true})
+
+const Orders = mongoose.model("Order",orderSchema)
+export default Orders
